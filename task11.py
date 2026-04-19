5256cffb694b39b09f5d60f43e53a49799c26f8e
ali mekhridinov <mehriddinovali19@gmail.com>
2026-04-19 15:39:15 +0500

    task11 done

--- task11.py
+++ task11.py
@@ -3,7 +3,7 @@ from products import products
 min_price = 50
 max_price = 100
 
-res = list(map(lambda p: p["name"],filter(lambda p: any(min_price <= v["price"] <= max_price
+res = list(map(lambda p: p["name"],filter(lambda p: any( min_price <= v["price"] <= max_price
             for v in p["variants"]),products)))
 
 print(res)
